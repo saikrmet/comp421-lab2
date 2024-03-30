0cdad971af1f62c09854916eabdbd76b7e124a69
Albert Wan <111791504+albert-wan-at-rice@users.noreply.github.com>
2024-03-29 21:24:19 -0500

    Create trapHandling.h

---
+++ trapHandling.h
@@ -0,0 +1,18 @@
+#include <comp421/hardware.h>
+#include <comp421/yalnix.h>
+
+void get_pid_handler(ExceptionInfo *exInfo);
+void wait_handler(ExceptionInfo *exInfo);
+void exit_handler(ExceptionInfo *exInfo, int wasProgramErr);
+void exec_handler(ExceptionInfo *exInfo);
+void fork_handler(ExceptionInfo *exInfo);
+void delay_handler(ExceptionInfo *exInfo);
+void tty_read_handler(ExceptionInfo *exInfo);
+void tty_write_handler(ExceptionInfo *exInfo);
+
+void trap_tty_receive_handler(ExceptionInfo *exInfo);
+void trap_tty_transmit_handler(ExceptionInfo *exInfo);
+void trap_clock_handler(ExceptionInfo *exInfo);
+void trap_illegal_handler(ExceptionInfo *exInfo);
+void trap_math_handler(ExceptionInfo *exInfo);
+void trap_memory_handler(ExceptionInfo *exInfo);
