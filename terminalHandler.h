2c73a71e191891fe257b244f54ddfa1ee66cac8f
Albert Wan <111791504+albert-wan-at-rice@users.noreply.github.com>
2024-03-29 21:28:15 -0500

    Create terminalHandler.h

---
+++ terminalHandler.h
@@ -0,0 +1,17 @@
+#include <comp421/hardware.h>
+#include <comp421/yalnix.h>
+#include <stdlib.h>
+
+struct bufStruct *bufs;
+
+struct bufStruct {
+    char term_buffer[TERMINAL_MAX_LINE];
+    int size;
+    int head;
+    int tail;
+    
+};
+int newline(int terminal);
+void createBufs();
+int read(char *buffer, int terminal, int size);
+int write(char *buffer, int terminal, int blocked, int size);
