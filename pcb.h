74a3aaa117b8298bad803c52c920cb7ac4d0c188
Albert Wan <111791504+albert-wan-at-rice@users.noreply.github.com>
2024-03-29 21:31:07 -0500

    Create pcb.h

---
+++ pcb.h
@@ -0,0 +1,32 @@
+#include <comp421/hardware.h>
+#include <comp421/yalnix.h>
+
+struct pcbStruct
+{
+  int pid;
+  struct pte *pcbPT;
+  SavedContext sc;
+  int delayTicks;
+  void *brk;
+  void *stackSize;
+  int blockProcess;
+  int children;
+  int parentPid;
+  int termReading;
+  int callRead;
+  int termProducing;
+  int callProduce;
+  struct terminateEntry *terminateProcess; 
+};
+
+struct terminateEntry
+{
+    int status;
+    int pid;
+    struct terminateEntry *next;
+};
+
+struct pcbStruct* fetchPcb(int old_pid);
+struct pcbStruct* createPcb(int pid, int parentPid, struct pcbStruct* pcb);
+struct terminateEntry* removeTerminatedEntry(struct pcbStruct* data);
+void addTerminatedEntry(struct pcbStruct* pcb, int pid, int status);
